852314a15fa0e9b97b297e5d316573e376d1c4d3
Lynda Benabdessadok <lylyben74@gmail.com>
2023-01-26 14:58:53 +0100

    init socket

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,23 @@
-console.log("ALLO TEST")
+const express = require('express');
+// Creation du Serveur
+const app = express();
+const {Server} = require('socket.io');
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin : "*",
+    }
+})
+
+// Initialisation
+app.use(express.json())
+app.use(cors())
+
+// Socket (requètes des messages)
+io.on('connection', (socket) =>{
+    socket.on('message', (data) =>{
+        socket.emit('sendFront', data)
+        console.log(data)
+    })
+})
+
