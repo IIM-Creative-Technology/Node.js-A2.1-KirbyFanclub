f582620386f137f82f3bb2419df5272fd1cc3ec0
Sarayeo <yeokpeusseusarafiela@gmail.com>
2023-01-27 15:15:31 +0100

    :sparkles:adding style

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,29 @@
 const express = require('express');
 const cors = require('cors');
-const cors = require('cors');
 const http = require('http');
-require('dotenv').config();
-const mongoose = require('mongoose');
-const { MongoClient } = require('mongodb');
-const client = new MongoClient(process.env.MONGO_URL);
 
-
-
-async function main(){
-    await client.connect();
-    console.log('CA SE CONNECTE')
-    const db = client.db('test') //création d'une database 'test'
-    const collection = db.collection('allo'); //on récupère les collections
-    const insertStuff = await collection.insertMany([{ a : 1},{ b : 2},{ c : 3}]) //apres avoir recup les collections on insere
-    console.log('documents insérés =>', insertStuff)
-    return 'slay';
-}
-
-main()
-.then(console.log)
-.catch(console.error)
-.finally(() => client.close());
-
+const {Server} = require('socket.io')
 
 const app = express();
 const port = 3000;
 
-const server = http.createServer(app)
+
+const httpServer = http.createServer(app)
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: "*",
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log(socket.id)
+    socket.on('message', (data) => {
+        console.log(data)
+        io.emit('sendFront', data)
+    })
+})
+
 
 // middlewares
 app.use(express.json())
@@ -71,6 +66,18 @@ app.delete('/api/user', (req, res) => {
     res.send('user a été supprimé')
 })
 
+// app.delete('/api/user/:id', (req, res) => {
+//     const userId = req.params.id;
+//     Resource.findByIdAndRemove(userId, (err, resource) => {
+//         if (err) {
+//             res.status(500).send(err);
+//         } else {
+//             res.send(`L'utilisateur ${userId} a été supprimé`);
+//         }
+//     });
+// });
+// Resource correspond à un modèle Mongoose prédéfinit auparavant
+
 
 // chat
 app.get('/api/chat/messages', (req, res) => {
@@ -94,8 +101,19 @@ app.delete('/api/chat/messages/:id', (req, res) => {
     res.send('Le message a été supprimé')
 })
 
+// app.delete('/api/chat/messages/:id', (req, res) => {
+//     const messageId = req.params.id;
+//     Resource.findByIdAndRemove(messageId, (err, resource) => {
+//         if (err) {
+//             res.status(500).send(err);
+//         } else {
+//             res.send(`Le message ${messageId} a été supprimé`);
+//         }
+//     });
+// });
 
 
-server.listen(port, () => {
+
+httpServer.listen(port, () => {
     console.log(`tout est ok, on écoute sur le port ${port}`)
 })
