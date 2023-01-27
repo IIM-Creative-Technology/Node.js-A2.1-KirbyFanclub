207b1b94f49f9add54d1e6ff627be2c97619f702
Sarayeo <yeokpeusseusarafiela@gmail.com>
2023-01-27 11:14:12 +0100

    :sparkles:

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,119 @@
 const express = require('express');
+const cors = require('cors');
 const http = require('http');
+
+const {Server} = require('socket.io')
+
 const app = express();
 const port = 3000;
 
-const cors= require('cors');
+
+const httpServer = http.createServer(app)
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: "*",
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log(socket.id)
+    socket.on('message', (data) => {
+        console.log(data)
+        io.emit('sendFront', data)
+    })
+})
+
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+
+// ------ routes ------
+// user
+app.get('/api/user', (req, res) => {
+    console.log(req.params)
+    res.json('{msg: info user}')
+})
+
+// inscription 
+app.post('/api/user/signup', (req, res) => {
+    console.log(req.body)
+    res.json(req.body)
+})
+
+// login
+app.post('/api/user/login', (req, res) => {
+    console.log(req.body)
+    res.json(req.body)
+})
+
+// déconnexion
+app.get('/api/user/logout', (req, res) => {
+    console.log(req.params)
+    res.json('{msg: deconnexion du user}')
+})
+
+// réinitialisation du mdp
+app.post('/api/user/forget', (req, res) => {
+    console.log(req.body)
+    res.json(req.body)
+})
+
+// suppression du user
+app.delete('/api/user', (req, res) => {
+    res.send('user a été supprimé')
+})
+
+// app.delete('/api/user/:id', (req, res) => {
+//     const userId = req.params.id;
+//     Resource.findByIdAndRemove(userId, (err, resource) => {
+//         if (err) {
+//             res.status(500).send(err);
+//         } else {
+//             res.send(`L'utilisateur ${userId} a été supprimé`);
+//         }
+//     });
+// });
+// Resource correspond à un modèle Mongoose prédéfinit auparavant
+
+
+// chat
+app.get('/api/chat/messages', (req, res) => {
+    console.log(req.params)
+    res.json('{msg: recuperation de tous les messages}')
+})
+
+app.get('/api/chat/:id', (req, res) => {
+    console.log(req.params)
+    res.json('{msg: recuperation des messages du user}')
+})
+
+// créer un message
+app.post('/api/chat/messages', (req, res) => {
+    console.log(req.body)
+    res.json(req.body)
+})
+
+// supprimer un message
+app.delete('/api/chat/messages/:id', (req, res) => {
+    res.send('Le message a été supprimé')
+})
+
+// app.delete('/api/chat/messages/:id', (req, res) => {
+//     const messageId = req.params.id;
+//     Resource.findByIdAndRemove(messageId, (err, resource) => {
+//         if (err) {
+//             res.status(500).send(err);
+//         } else {
+//             res.send(`Le message ${messageId} a été supprimé`);
+//         }
+//     });
+// });
+
+
+
+httpServer.listen(port, () => {
+    console.log(`tout est ok, on écoute sur le port ${port}`)
+})
---
+++ frontend/script.js
@@ -0,0 +1,27 @@
+const btnSendMes = document.getElementById('submitMessage')
+
+// const socketMessage = io('http://localhost:3000/api/chat/messages/');
+const socket = io('http://localhost:3000');
+
+const divMessages = document.getElementById('messages');
+
+
+// var value;
+
+btnSendMes.addEventListener('click', () => {
+    // inputValue.addEventListener('input', () => {
+    //     value = this.value;
+    // })
+
+    let inputValue = document.getElementById('insertMessage').value;
+    socket.emit('message', {msgChat: `${inputValue}`})
+    console.log(inputValue)
+});
+
+socket.on('sendFront', (data) => {
+    const p = document.createElement('p')
+    p.innerText = data.msgChat
+
+    console.log(data)
+    divMessages.appendChild(p)
+})
