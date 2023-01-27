276ef0fd90e673f7a6d7a00e0394fe7f2dffb1ed
Sixtine Delvallée <sixtine.delvallee@gmail.com>
2023-01-27 11:20:29 +0100

    commit before merge

--- frontend/script.js
+++ frontend/script.js
@@ -1,13 +1,13 @@
-// const btnMessage = document.getElementById('submitMessage');
+const btnMessage = document.getElementById('submitMessage');
 
-// btnMessage.addEventListener('click', () => {
-//     fetch("http://localhost:3000/api/chat/messages/", {
-//         method: "POST",
-//         headers: {
-//             'Content-Type': 'application/json;charset=utf-8'
-//         },
-//         body: JSON.stringify({
-//             messageContent: ""
-//         })
-//     })
-// })
+btnMessage.addEventListener('click', () => {
+    fetch("http://localhost:3000/", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({
+            messageContent: ""
+        })
+    })
+})
