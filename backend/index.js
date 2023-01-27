c711fb24759d7841c7f9503a750c90968b9b1a54
Hoai-lan DINH VU <christiane.dinhvu@gmail.com>
2023-01-27 11:43:57 +0100

    futur dossier

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ async function main(){
     await client.connect();
     console.log('CA SE CONNECTE')
     const db = client.db('test') //création d'une database 'test'
-    const collection = db.collection('documents'); //on récupère les collections
+    const collection = db.collection('allo'); //on récupère les collections
     const insertStuff = await collection.insertMany([{ a : 1},{ b : 2},{ c : 3}]) //apres avoir recup les collections on insere
     console.log('documents insérés =>', insertStuff)
     return 'slay';
@@ -22,6 +22,7 @@ main()
 .then(console.log)
 .catch(console.error)
 .finally(() => client.close());
+
 
 const app = express();
 const port = 3000;
