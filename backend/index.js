2476fd6503bf8b8a3759394caadc3c7549981616
Sarayeo <yeokpeusseusarafiela@gmail.com>
2023-01-29 09:31:52 +0100

    :construction_worker:not really new things but tests

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const http = require('http');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
+const client = new MongoClient(process.env.MONGO_URL);
+
 
 const {Server} = require('socket.io')
 
@@ -23,6 +28,23 @@ io.on('connection', (socket) => {
         io.emit('sendFront', data)
     })
 })
+
+async function main(){
+    await client.connect();
+    console.log('CA SE CONNECTE')
+    const db = client.db('test') //création d'une database 'test'
+    const collection = db.collection('allo'); //on récupère les collections
+    const insertStuff = await collection.insertMany([{ a : 1},{ b : 2},{ c : 3}]) //apres avoir recup les collections on insere
+    console.log('documents insérés =>', insertStuff)
+    return 'slay';
+}
+
+main()
+.then(console.log)
+.catch(console.error)
+.finally(() => client.close());
+
+const server = http.createServer(app)
 
 
 // middlewares
