bda5f24c3ebf1f78fbc7f8f1244c997fd4d5f044
Hoai-lan DINH VU <christiane.dinhvu@gmail.com>
2023-01-29 23:42:23 +0100

    try form to mongodb

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const User = require('../models/user');
 const router = new express.Router();
 
-router.post('/users', async (req, res, next) => {
+router.post('/signup', async (req, res, next) => {
     const user = new User(req.body);
 
     try{
